b1c5aecda63af0de5d5e892ae966bf09f4beb855
LOIC-PC-22\lleprieur <leprieurloic@gmail.com>
2023-04-28 01:44:55 +0200

    Import de Route

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Routing\Annotation\Route;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../../.env');
